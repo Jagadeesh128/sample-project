00fd9241a08669472fdf45090199164a10b887d0
G  Jagadeesh <goodalavadijagadeesh@gmail.com>
2025-12-14 19:00:06 +0530

    git rebase change 1 try1

--- src/app.js
+++ src/app.js
@@ -3,3 +3,4 @@ console.log('another line added on hotfix/fix-typo branch');
 console.log('changes from feature/header branch');
 console.log('Git stash checking 2...');
 console.log('Rebase change 1');
+console.log('Rebase change 1 try 1');
