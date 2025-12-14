29f8ce3b4f7a626629cec228137b513ca091a0c0
G  Jagadeesh <goodalavadijagadeesh@gmail.com>
2025-12-14 19:18:15 +0530

    Rebase test

--- src/app.js
+++ src/app.js
@@ -4,3 +4,4 @@ console.log('changes from feature/header branch');
 console.log('Git stash checking 2...');
 console.log('Rebase change 1');
 console.log('Rebase change 1 try 1');
+console.log('Rebase change from develop2');
